cd9253b1328e7019818f7686d3cef6c8803711c6
Arjen van Bochoven <arjen@bochoven.net>
2025-12-06 13:03:26 +0100

    Add exceptions

---
+++ src/Exceptions/ApiException.php
@@ -0,0 +1,24 @@
+<?php
+// filepath: /Volumes/Development/pe-online/src/Exceptions/ApiException.php
+
+namespace PeOnline\Exceptions;
+
+/**
+ * Exception thrown when API errors occur
+ */
+class ApiException extends \Exception
+{
+    private int $apiErrorCode;
+
+    public function __construct(string $message = '', int $code = 0, ?\Throwable $previous = null)
+    {
+        $this->apiErrorCode = $code;
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function getApiErrorCode(): int
+    {
+        return $this->apiErrorCode;
+    }
+
+}
---
+++ src/Exceptions/ValidationException.php
@@ -0,0 +1,11 @@
+<?php
+// filepath: /Volumes/Development/pe-online/src/Exceptions/ValidationException.php
+
+namespace PeOnline\Exceptions;
+
+/**
+ * Exception thrown when validation errors occur
+ */
+class ValidationException extends \Exception
+{
+}
